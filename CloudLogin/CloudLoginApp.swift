6796cf10571081b9c8a71effd80c72a9e951d66d
Ashlee Muscroft <ashlee.muscroft@>
2021-06-04 13:20:03 +0100

    Initial Commit

---
+++ CloudLogin/CloudLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudLoginApp.swift
+//  CloudLogin
+//
+//  Created by Ashlee Muscroft on 04/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct CloudLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
